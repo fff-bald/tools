62e6e03ca1a219b3e796689cb40bea0f838c7ba4
cjl <1060021119@qq.com>
2024-08-05 00:06:25 +0800

    代码整理

--- src/main/java/fund/constant/FundConstant.java
+++ src/main/java/fund/constant/FundConstant.java
@@ -11,15 +11,15 @@ public interface FundConstant {
     String START_DATE = "1970-01-01";
 
     // 每日数据获取链接模板
-    String FUND_DAY_CHANGE_URL = "https://fundf10.eastmoney.com/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=%s";
+    String FUND_DAY_CHANGE_URL = "https://for.example.com/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=%s";
 
     // 全量基金ID数据存在一个js文件里
-    String ALL_FUND_IDS_URL = "https://fund.eastmoney.com/js/fundcode_search.js";
+    String ALL_FUND_IDS_URL = "https://for.example.js";
 
     // 全量基金ID数据存在一个js文件里
-    String FUND_DATA_GET_URL = "https://fund.eastmoney.com/%s.html";
+    String FUND_DATA_GET_URL = "https://for.example.html";
 
-    String FILE_ABSOLUTE_PATH = "C:\\Users\\Administrator\\Desktop\\FundData\\%s.csv";
+    String FILE_ABSOLUTE_PATH = "C:\\FundData\\%s.csv";
 
     String LOG_NAME = "fund";
 }
