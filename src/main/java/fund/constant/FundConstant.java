6c05797e664a21ec2ac77612bf9c7dea24b266be
cjl <1060021119@qq.com>
2024-08-24 18:57:28 +0800

    重构 魔法数

--- src/main/java/fund/constant/FundConstant.java
+++ src/main/java/fund/constant/FundConstant.java
@@ -10,17 +10,17 @@ public interface FundConstant {
 
     String START_DATE = "1970-01-01";
 
+    // 全量基金ID数据存在一个js文件里
+    String ALL_FUND_IDS_URL = "https://for.example.com";
+
+    // 基金资料获取链接模板
+    String FUND_DATA_GET_URL = "https://for.example.com";
+
     // 每日数据获取链接模板
     String FUND_DAY_CHANGE_URL = "https://for.example.com";
 
     // 持有人結構获取链接模板
     String OCCUPY_PROPORTION_URL = "https://for.example.com";
-
-    // 全量基金ID数据存在一个js文件里
-    String ALL_FUND_IDS_URL = "https://for.example.com";
-
-    // 全量基金ID数据存在一个js文件里
-    String FUND_DATA_GET_URL = "https://for.example.com";
 
     String FILE_ABSOLUTE_PATH = "C:\\FundData\\%s.csv";
 }
